4640b0d86ed37174718b13da734edf3794dc4984
Tony <nya.tony2010@gmail.com>
2023-02-27 14:53:20 +0100

    update

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -13,7 +13,7 @@ class User():
     - password: private string hash in MD5
     """
 
-    __password = None
+    _password = None
 
     def __init__(self):
         """
@@ -27,7 +27,7 @@ class User():
         """
         Password getter
         """
-        return self.__password
+        return self._password
 
     @password.setter
     def password(self, pwd):
@@ -35,10 +35,10 @@ class User():
         Password setter:
         - `None` if `pwd` is `None`
         - `None` if `pwd` is not a string
-        - Hash `pwd` in MD5 before assign to `__password`
+        - Hash `pwd` in MD5 before assign to `_password`
         """
         if pwd is None or type(pwd) is not str:
-            self.__password = None
+            self._password = None
         else:
             self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
@@ -47,14 +47,14 @@ class User():
         Valid password:
         - `False` if `pwd` is `None`
         - `False` if `pwd` is not a string
-        - `False` if `__password` is `None`
-        - Compare `__password` and the MD5 value of `pwd`
+        - `False` if `_password` is `None`
+        - Compare `_password` and the MD5 value of `pwd`
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        if self._password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self._password
 
 
 if __name__ == '__main__':
