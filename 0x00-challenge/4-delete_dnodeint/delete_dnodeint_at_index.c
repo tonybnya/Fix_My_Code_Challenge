e95ac01968c2bbe8836d587f85d10c708f1633a9
Tony <nya.tony2010@gmail.com>
2023-03-13 20:16:09 +0100

    Fix: delete_dnodeint_at_index.c

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,43 +11,38 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+	dlistint_t *current = *head, *temp;
+	unsigned int i;
 
 	if (*head == NULL)
 	{
 		return (-1);
 	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->next->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
+
+        if (index == 0) {
+                *head = (*head)->next;
+                if (*head != NULL)
+                        (*head)->prev = NULL;
+                free(current);
+                return (-1);
+        }
+
+        for (i = 0; i < index; i++) {
+                if (current == NULL)
+                        return (-1);
+                current = current->next;
+        }
+
+        if (current == NULL)
+                return (-1);
+
+        temp = current->prev;
+        temp->next = current->next;
+
+        if (current->next != NULL)
+                current->next->prev = temp;
+
+        free(current);
+
 	return (1);
 }
