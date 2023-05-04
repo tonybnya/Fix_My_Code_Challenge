f2e8c1c8f6d1566ff50f310743554ab6d4339271
Tony <nya.tony2010@gmail.com>
2023-05-04 15:19:31 +0100

    Update: square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -15,7 +15,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
